9894c26209aef66a0f1d33a9b78094816e3f2987
Mohamed Amine Lakhal <mohamedamine.lakhal@se.linedata.com>
2023-03-28 13:26:24 +0100

    get info

--- src/main/java/com/pluralsight/order/dao/GetOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/GetOrderDao.java
@@ -1,69 +1,81 @@
 package com.pluralsight.order.dao;
 
-import com.pluralsight.order.dto.OrderDto;
-import com.pluralsight.order.dto.ParamsDto;
-import com.pluralsight.order.util.Database;
-import com.pluralsight.order.util.ExceptionHandler;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.pluralsight.order.dto.OrderDto;
+import com.pluralsight.order.dto.ParamsDto;
+import com.pluralsight.order.util.Database;
+import com.pluralsight.order.util.ExceptionHandler;
+
 /**
  * DAO to get an order
  */
-public class GetOrderDao {
-    private String query = "SELECT * FROM orders o WHERE o.order_id = ?";
-    private Database database;
+public class GetOrderDao
+{
+   private String   query = "SELECT * FROM orders o WHERE o.order_id = ?";
+   private Database database;
 
-    /**
-     * Constructor
-     * @param database Database object
-     */
-    public GetOrderDao(Database database) {
-        this.database = database;
-    }
+   /**
+    * Constructor
+    * 
+    * @param database Database object
+    */
+   public GetOrderDao(Database database)
+   {
+      this.database = database;
+   }
 
-    /**
-     * Gets an order by its ID
-     * @param paramsDto Object with the parameters for the operation
-     * @return Object with the main information of an order
-     */
-    public OrderDto getOrderById(ParamsDto paramsDto) {
-        OrderDto orderDto = null;
+   /**
+    * Gets an order by its ID
+    * 
+    * @param paramsDto Object with the parameters for the operation
+    * @return Object with the main information of an order
+    */
+   public OrderDto getOrderById(ParamsDto paramsDto)
+   {
+      OrderDto orderDto = null;
 
-        try (Connection con = null;
-             PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderId());
-             ResultSet rs = createResultSet(ps)
-        ) {
+      try (Connection con = database.getConnection(); PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderId()); ResultSet rs = createResultSet(ps))
+      {
 
-        } catch (SQLException ex) {
-            ExceptionHandler.handleException(ex);
-        }
+      }
+      catch (SQLException ex)
+      {
+         ExceptionHandler.handleException(ex);
+      }
 
-        return orderDto;
-    }
+      return orderDto;
+   }
 
-    /**
-     * Creates a PreparedStatement object to get an order
-     * @param con Connnection object
-     * @param orderId Order ID to set on the PreparedStatement
-     * @return A PreparedStatement object
-     * @throws SQLException In case of an error
-     */
-    private PreparedStatement createPreparedStatement(Connection con, long orderId) throws SQLException {
+   /**
+    * Creates a PreparedStatement object to get an order
+    * 
+    * @param con Connnection object
+    * @param orderId Order ID to set on the PreparedStatement
+    * @return A PreparedStatement object
+    * @throws SQLException In case of an error
+    */
+   private PreparedStatement createPreparedStatement(Connection con, long orderId) throws SQLException
+   {
 
-        return null;
-    }
+      PreparedStatement ps = con.prepareStatement(query);
+      ps.setLong(1, orderId);
+      return ps;
 
-    /**
-     * Creates a ResultSet object to get the results of the query
-     * @param ps PreparedStatement object to create the query
-     * @return A ResultSet object
-     * @throws SQLException In case of an error
-     */
-    private ResultSet createResultSet(PreparedStatement ps) throws SQLException {
-        return null;
-    }
+   }
+
+   /**
+    * Creates a ResultSet object to get the results of the query
+    * 
+    * @param ps PreparedStatement object to create the query
+    * @return A ResultSet object
+    * @throws SQLException In case of an error
+    */
+   private ResultSet createResultSet(PreparedStatement ps) throws SQLException
+   {
+      return ps.executeQuery();
+   }
 }
