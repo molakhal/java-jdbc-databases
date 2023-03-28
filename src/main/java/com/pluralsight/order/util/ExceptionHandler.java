8acdcea39e4a7506875f83f21a5942355437e98c
Mohamed Amine Lakhal <mohamedamine.lakhal@se.linedata.com>
2023-03-28 12:01:37 +0100

    ExceptionHandler

--- src/main/java/com/pluralsight/order/util/ExceptionHandler.java
+++ src/main/java/com/pluralsight/order/util/ExceptionHandler.java
@@ -5,13 +5,21 @@ import java.sql.SQLException;
 /**
  * Utility class to handle exceptions
  */
-public class ExceptionHandler {
+public class ExceptionHandler
+{
 
-    /**
-     * Method to extract and print information from a SQLException
-     * @param sqlException Exception from which information will be extracted
-     */
-    public static void handleException(SQLException sqlException) {
+   /**
+    * Method to extract and print information from a SQLException
+    * 
+    * @param sqlException Exception from which information will be extracted
+    */
+   public static void handleException(SQLException sqlException)
+   {
 
-    }
+      System.out.println("Error code: " + sqlException.getErrorCode());
+      System.out.println("SQL state: " + sqlException.getSQLState());
+      System.out.println("Message: " + sqlException.getMessage());
+      sqlException.printStackTrace();
+
+   }
 }
